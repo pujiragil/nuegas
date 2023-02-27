c1ee1410dcf45252004910cce19a9204cb97e750
pujiragil <ragilsg164@gmail.com>
2023-02-27 22:05:06 +0700

    added secondary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     primary: {
       100: "#FFFFFF",
       200: "#FAFAFA",
-      300: "#F5F5F7"
+      300: "#F5F5F7",
+    },
+    secondary: {
+      100: "#141522",
+      200: "#54577A",
+      300: "#8E92BC",
     },
     extend: {},
   },
   plugins: [],
-}
+};
