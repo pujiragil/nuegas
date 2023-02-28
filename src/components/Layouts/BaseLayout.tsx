87e03639bb4397db2e3984f7f72230451213a98f
pujiragil <ragilsg164@gmail.com>
2023-02-28 21:26:17 +0700

    apply navbar component

--- src/components/Layouts/BaseLayout.tsx
+++ src/components/Layouts/BaseLayout.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Navbar from "../Navbar";
 
 const BaseLayout = () => {
-  return <div>BaseLayout</div>;
+  return (
+    <div className="max-w-screen-2xl mx-auto px-6 py-8 md:px-8 bg-primary-100">
+      <div className="grid lg:grid-cols-3">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
 export default BaseLayout;
