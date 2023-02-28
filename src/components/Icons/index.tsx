78e0c2a246224155febe6d547f73979b09587b21
pujiragil <ragilsg164@gmail.com>
2023-02-28 21:22:48 +0700

    created component icons

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,5 @@
+export { default as Overview } from "./Overview";
+export { default as Task } from "./Task";
+export { default as Mentor } from "./Mentor";
+export { default as Message } from "./Message";
+export { default as Setting } from "./Setting";
