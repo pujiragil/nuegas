05052f42c5f24f7647669b59647f815b5269212d
pujiragil <ragilsg164@gmail.com>
2023-02-28 10:20:58 +0700

    fix: replace exisiting custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,23 +2,27 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    primary: {
-      100: "#FFFFFF",
-      200: "#FAFAFA",
-      300: "#F5F5F7",
-    },
-    secondary: {
-      100: "#141522",
-      200: "#54577A",
-      300: "#8E92BC",
-    },
-    tertiary: {
-      100: "#546FFF",
-      200: "#BAC8FF",
+    colors: {
+      primary: {
+        100: "#FFFFFF",
+        200: "#FAFAFA",
+        300: "#F5F5F7",
+      },
+      secondary: {
+        100: "#141522",
+        200: "#54577A",
+        300: "#8E92BC",
+      },
+      tertiary: {
+        100: "#546FFF",
+        200: "#BAC8FF",
+      },
     },
     extend: {
       maxWidth: {
-        "2xl": "1440px",
+        screen: {
+          "2xl": "1440px",
+        },
       },
     },
   },
