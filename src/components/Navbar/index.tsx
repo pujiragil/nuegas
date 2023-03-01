4f1b700062e7e511bb305fd207b1ca1414548b88
pujiragil <ragilsg164@gmail.com>
2023-03-01 13:49:48 +0700

    applied navList and navMobile component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,32 +1,23 @@
 import { ReactComponent as Nuegas } from "../../assets/nuegas.svg";
-import NavLink from "./NavLink";
-import { Overview, Task, Mentor, Message, Setting } from "../Icons";
+import NavList from "./NavList";
+import NavMobile from "./NavMobile";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-col gap-16 w-6/12">
-      <div className="flex gap-3 items-center">
-        <Nuegas />
-        <h1 className="font-semibold text-secondary-100 text-[32px]">Nuegas</h1>
-      </div>
-      <ul className="grid gap-6">
-        <li>
-          <NavLink href="/" icon={Overview} name="Overview" />
-        </li>
-        <li>
-          <NavLink href="/task" icon={Task} name="Task" />
-        </li>
-        <li>
-          <NavLink href="/mentors" icon={Mentor} name="Mentors" />
-        </li>
-        <li>
-          <NavLink href="/message" icon={Message} name="Message" />
-        </li>
-        <li>
-          <NavLink href="/settings" icon={Setting} name="Settings" />
-        </li>
-      </ul>
-    </nav>
+    <>
+      {/* Navbar for tablet and desktop */}
+      <nav className="hidden md:flex flex-col gap-16 p-8 border-r border-primary-300 bg-primary-100 w-fit">
+        <div className="flex gap-3 items-center">
+          <Nuegas className="w-10 h-10" />
+          <h1 className="font-semibold text-secondary-100 text-[32px]">
+            Nuegas
+          </h1>
+        </div>
+        <NavList />
+      </nav>
+      {/* Navbar for mobile */}
+      <NavMobile />
+    </>
   );
 };
 
