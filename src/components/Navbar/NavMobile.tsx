50627d755ed1c5fea948be72a7cef481398a5b40
pujiragil <ragilsg164@gmail.com>
2023-03-01 22:00:01 +0700

    made responsive navMobile component

--- src/components/Navbar/NavMobile.tsx
+++ src/components/Navbar/NavMobile.tsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+import NavList from "./NavList";
+
 const iconStyle: string =
   "w-11 h-11 p-3 rounded-full border border-primary-300 cursor-pointer hover:bg-primary-300";
 
 const NavMobile = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <nav className="px-6 py-8 md:hidden">
+    <nav className="px-6 py-8 border-b border-primary-300 relative md:hidden">
       <div className="flex justify-between">
         {/* Hamburger Navigation */}
-        <div className={iconStyle}>
-          <img
-            className="w-10 h-auto object-cover cursor-pointer"
-            src="/hamburger.svg"
-            alt="hamburger"
-          />
+        <div
+          className={iconStyle}
+          onClick={() => setIsOpen((prevOpen) => !prevOpen)}
+        >
+          {isOpen ? (
+            <img
+              className="w-6 h-auto object-cover cursor-pointer"
+              src="close-hamburger.svg"
+              alt="close-hamburger"
+            />
+          ) : (
+            <img
+              className="w-6 h-auto object-cover cursor-pointer"
+              src="/hamburger.svg"
+              alt="hamburger"
+            />
+          )}
         </div>
 
         {/* Profile Navigation */}
@@ -31,6 +47,12 @@ const NavMobile = () => {
           />
         </div>
       </div>
+
+      {isOpen && (
+        <div className="mobile-navlist">
+          <NavList />
+        </div>
+      )}
     </nav>
   );
 };
