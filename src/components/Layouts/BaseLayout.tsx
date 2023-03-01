6a8ef9c691f48825d98ed6745acfaf28f03d5609
pujiragil <ragilsg164@gmail.com>
2023-03-01 22:02:05 +0700

    applied pageLayout component in baseLayout

--- src/components/Layouts/BaseLayout.tsx
+++ src/components/Layouts/BaseLayout.tsx
@@ -1,10 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
+import PageLayout from "./PageLayout";
 
 const BaseLayout = () => {
   return (
-    <div className="max-w-screen-2xl mx-auto px-6 py-8 md:px-8 bg-primary-100">
-      <div className="grid lg:grid-cols-3">
+    <div className="max-w-screen-2xl mx-auto bg-primary-100">
+      <div className="flex flex-col md:flex-row">
         <Navbar />
+        <PageLayout>
+          <Outlet />
+        </PageLayout>
       </div>
     </div>
   );
