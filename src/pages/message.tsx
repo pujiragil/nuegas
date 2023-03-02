10de7999a6b300d987880e6965f38646b1e6e35c
pujiragil <ragilsg164@gmail.com>
2023-03-02 15:03:40 +0700

    pages: created message page

---
+++ src/pages/message.tsx
@@ -0,0 +1,11 @@
+import ProfileLayout from "../components/Layouts/ProfileLayout";
+
+const Message = () => {
+  return (
+    <>
+      <ProfileLayout titleProfile="Message" />
+    </>
+  );
+};
+
+export default Message;
