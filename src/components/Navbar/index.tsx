8774c7231431949a44e841cb3161cb8041392eaf
pujiragil <ragilsg164@gmail.com>
2023-03-02 14:48:00 +0700

    removed navMobile component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,22 +1,18 @@
-import { ReactComponent as Nuegas } from "../../assets/nuegas.svg";
 import NavList from "./NavList";
-import NavMobile from "./NavMobile";
+import nuegas from "../../assets/nuegas.svg";
 
 const Navbar = () => {
   return (
     <>
-      {/* Navbar for tablet and desktop */}
       <nav className="hidden md:flex flex-col gap-16 p-8 border-r border-primary-300 bg-primary-100 w-fit">
         <div className="flex gap-3 items-center">
-          <Nuegas className="w-10 h-10" />
+          <img className="w-10 h-10 object-cover" src={nuegas} alt="nuegas" />
           <h1 className="font-semibold text-secondary-100 text-[32px]">
             Nuegas
           </h1>
         </div>
         <NavList />
       </nav>
-      {/* Navbar for mobile */}
-      <NavMobile />
     </>
   );
 };
