597cde7264a13f74fce54577a7b473bef3766c27
pujiragil <ragilsg164@gmail.com>
2023-03-02 15:03:01 +0700

    pages: created mentors page

---
+++ src/pages/mentors.tsx
@@ -0,0 +1,11 @@
+import ProfileLayout from "../components/Layouts/ProfileLayout";
+
+const Mentors = () => {
+  return (
+    <>
+      <ProfileLayout titleProfile="Explore Mentors" />
+    </>
+  );
+};
+
+export default Mentors;
