e7748fc25932034c2dad2b6ff5935a26e6dc54d8
pujiragil <ragilsg164@gmail.com>
2023-03-02 15:03:24 +0700

    pages: created settings page

---
+++ src/pages/settings.tsx
@@ -0,0 +1,11 @@
+import ProfileLayout from "../components/Layouts/ProfileLayout";
+
+const Settings = () => {
+  return (
+    <>
+      <ProfileLayout titleProfile="Settings" />
+    </>
+  );
+};
+
+export default Settings;
