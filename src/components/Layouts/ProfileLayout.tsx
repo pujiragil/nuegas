3888cf559561268accf703abc203f743eb823fdd
pujiragil <ragilsg164@gmail.com>
2023-03-02 14:47:08 +0700

    created profileLayout component

---
+++ src/components/Layouts/ProfileLayout.tsx
@@ -0,0 +1,35 @@
+import { FC, ReactElement, ReactNode } from "react";
+import ProfileNavigation from "../Profile/ProfileNavigation";
+
+interface ProfileLayoutProps {
+  titleProfile: string;
+  descProfile?: string;
+  children?: ReactNode;
+}
+
+const ProfileLayout: FC<ProfileLayoutProps> = ({
+  titleProfile,
+  descProfile,
+  children,
+}): ReactElement => {
+  return (
+    <div className="bg-primary-100 flex flex-col gap-8 border-b border-primary-300 md:p-8 md:gap-6 md:border-none">
+      <div className="flex flex-col gap-8 md:gap-0 md:flex-row md:justify-between md:items-center">
+        <div className="px-6 pb-8 space-y-2 order-2 md:order-1 md:p-0">
+          <h1 className="text-secondary-100 font-semibold text-2xl">
+            {titleProfile}
+          </h1>
+          {descProfile && (
+            <p className="font-medium text-secondary-200">{descProfile}</p>
+          )}
+        </div>
+
+        <ProfileNavigation />
+      </div>
+
+      {children}
+    </div>
+  );
+};
+
+export default ProfileLayout;
