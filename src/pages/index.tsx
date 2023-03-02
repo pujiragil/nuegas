fd8f41742b3e6f273ff8c730dfdf9c303208aa0f
pujiragil <ragilsg164@gmail.com>
2023-03-02 15:04:37 +0700

    pages: export all pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,6 @@
 export { default as Overview } from "./overview";
 export { default as Root } from "./root";
+export { default as Task } from "./task";
+export { default as Mentors } from "./mentors";
+export { default as Message } from "./message";
+export { default as Settings } from "./settings";
