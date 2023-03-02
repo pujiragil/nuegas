888d6826f80384183eca6b0dd5312fbaee7e6fe0
pujiragil <ragilsg164@gmail.com>
2023-03-02 15:02:44 +0700

    pages: created task page

---
+++ src/pages/task.tsx
@@ -0,0 +1,11 @@
+import ProfileLayout from "../components/Layouts/ProfileLayout";
+
+const Task = () => {
+  return (
+    <>
+      <ProfileLayout titleProfile="Explore Task"></ProfileLayout>
+    </>
+  );
+};
+
+export default Task;
