dbf91ceab3e098c7370693d2ff69113cb72a5fcf
pujiragil <ragilsg164@gmail.com>
2023-03-04 22:56:27 +0700

    created chart component

---
+++ src/components/Chart/index.tsx
@@ -0,0 +1,77 @@
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+
+type Datas = {
+  date: string;
+  task: number;
+};
+
+const datas: Array<Datas> = [
+  { date: "S", task: 4 },
+  { date: "M", task: 12 },
+  { date: "T", task: 5 },
+  { date: "W", task: 7 },
+  { date: "TH", task: 3 },
+  { date: "F", task: 6 },
+  { date: "ST", task: 2 },
+];
+
+const Chart = () => {
+  return (
+    <div className="flex flex-col gap-5 p-5 rounded-xl bg-primary-300 md:w-fit">
+      <div className="flex items-center justify-between">
+        <p>Activity</p>
+      </div>
+      <div className="w-full h-40 md:w-[422px] bg-primary-100 rounded-xl p-5">
+        <ResponsiveContainer>
+          <LineChart margin={{ left: 0, right: 0 }} data={datas}>
+            <Tooltip
+              content={<CustomTooltip />}
+              wrapperStyle={{
+                background: "none",
+                border: "none",
+                boxShadow: "none",
+              }}
+            />
+            <XAxis dataKey="date" />
+            <YAxis width={20} />
+            <Line
+              strokeWidth={3}
+              type="monotone"
+              dataKey="task"
+              stroke="#141522"
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+interface CustomTooltipProps {
+  active?: boolean;
+  payload?: any[];
+}
+
+const CustomTooltip = ({ active, payload }: CustomTooltipProps) => {
+  if (active && payload && payload.length) {
+    return (
+      <div
+        style={{ border: "none" }}
+        className="px-4 py-2 bg-secondary-100 rounded-xl text-primary-100 border-none"
+      >
+        <p>{payload[0].value} Task</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default Chart;
