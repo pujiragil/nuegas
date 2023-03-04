c426c6176fadaa7a385c3ceaa913c70be03993e5
pujiragil <ragilsg164@gmail.com>
2023-03-04 16:04:00 +0700

    created progress component

---
+++ src/components/Progress/types.ts
@@ -0,0 +1,56 @@
+import * as React from "react";
+
+export type CircularProgressbarStyles = {
+  root?: React.CSSProperties;
+  trail?: React.CSSProperties;
+  path?: React.CSSProperties;
+  text?: React.CSSProperties;
+  background?: React.CSSProperties;
+};
+
+export type CircularProgressbarDefaultProps = {
+  background: boolean;
+  backgroundPadding: number;
+  circleRatio: number;
+  classes: {
+    root: string;
+    trail: string;
+    path: string;
+    text: string;
+    background: string;
+  };
+  className: string;
+  counterClockwise: boolean;
+  maxValue: number;
+  minValue: number;
+  strokeWidth: number;
+  styles: CircularProgressbarStyles;
+  text: string;
+};
+
+// These are used for any CircularProgressbar wrapper components that can safely
+// ignore default props.
+export type CircularProgressbarWrapperProps = {
+  background?: boolean;
+  backgroundPadding?: number;
+  circleRatio?: number;
+  classes?: {
+    root: string;
+    trail: string;
+    path: string;
+    text: string;
+    background: string;
+  };
+  className?: string;
+  counterClockwise?: boolean;
+  maxValue?: number;
+  minValue?: number;
+  strokeWidth?: number;
+  styles?: CircularProgressbarStyles;
+  text?: string;
+  value: number;
+};
+
+export type CircularProgressbarProps = CircularProgressbarDefaultProps & {
+  value: number;
+};
