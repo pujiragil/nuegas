121fab39c7b8a3325804fa8e64db7f942b60aea2
pujiragil <ragilsg164@gmail.com>
2023-03-05 12:15:49 +0700

    created custom selected component

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,4 +1,5 @@
-import { FC, ReactElement } from "react";
+import arrow from "../../assets/arrow.svg";
+import { FC, ReactElement, useState } from "react";
 import {
   ResponsiveContainer,
   LineChart,
@@ -24,10 +25,38 @@ const datas: Array<Datas> = [
 ];
 
 const Chart = () => {
+  const [selectList, setSelectedList] = useState<string>("");
+  const lists: string[] = ["This Week", "Last Week"];
+
   return (
     <div className="flex flex-col gap-5 p-5 rounded-xl bg-primary-300 md:w-fit">
       <div className="flex items-center justify-between">
         <p>Activity</p>
+        <div className="text-xs relative">
+          <div className="flex items-center gap-2 cursor-pointer">
+            {selectList ? (
+              <>
+                <p>{selectList}</p>
+                <img src={arrow} alt={selectList} />
+              </>
+            ) : (
+              <>
+                <p>Select Week</p>
+                <img src={arrow} alt="arrow" />
+              </>
+            )}
+          </div>
+          <ul className="z-10 mt-2 left-0 absolute bg-primary-100 w-max border border-secondary-100 rounded-xl flex flex-col overflow-hidden">
+            {lists.map((list) => (
+              <li
+                onClick={() => setSelectedList(list)}
+                className="cursor-pointer p-2.5 hover:bg-tertiary-100 hover:text-primary-100 transition-all duration-200"
+              >
+                {list}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="w-full h-40 md:w-[422px] bg-primary-100 rounded-xl p-5">
         <ResponsiveContainer>
