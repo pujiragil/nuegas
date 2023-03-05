aa230cb5f74f125786a816f45c9bd7d4f16bd034
pujiragil <ragilsg164@gmail.com>
2023-03-05 22:27:05 +0700

    split up chart component

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,6 +1,6 @@
 import arrow from "../../assets/arrow.svg";
 import ActivityChart from "./AcitivityChart";
-import { useState } from "react";
+import React, { useState } from "react";
 
 type Datas = {
   day: string;
@@ -18,50 +18,60 @@ const datas: Array<Datas> = [
 ];
 
 const Chart = () => {
-  const [isShow, setIsShow] = useState<boolean>(false);
   const [selectList, setSelectedList] = useState<string>("");
-  const lists: string[] = ["This Week", "Last Week"];
 
   return (
     <div className="flex flex-col gap-5 p-5 rounded-xl bg-primary-300 md:w-fit">
-      <div className="flex items-center justify-between">
-        <p>Activity</p>
-        <div className="text-xs relative">
-          <div
-            onClick={() => setIsShow((prevShow) => !prevShow)}
-            className="flex items-center gap-2 cursor-pointer"
-          >
-            {selectList ? (
-              <>
-                <p>{selectList}</p>
-                <img src={arrow} alt={selectList} />
-              </>
-            ) : (
-              <>
-                <p>Select Week</p>
-                <img src={arrow} alt="arrow" />
-              </>
-            )}
-          </div>
-          {isShow && (
-            <ul className="z-10 mt-2 left-0 absolute bg-primary-100 w-max border border-secondary-100 rounded-xl flex flex-col overflow-hidden">
-              {lists.map((list) => (
-                <li
-                  onClick={() => {
-                    setSelectedList(list);
-                    setIsShow(false);
-                  }}
-                  className="cursor-pointer p-2.5 hover:bg-tertiary-100 hover:text-primary-100 transition-all duration-200"
-                >
-                  {list}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
+      <SelectTask select={selectList} onSelect={setSelectedList} />
       <div className="w-full h-40 md:w-[422px] bg-primary-100 rounded-xl p-5">
         <ActivityChart datas={datas} />
+      </div>
+    </div>
+  );
+};
+
+interface SelectTaskProps {
+  select: string;
+  onSelect: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SelectTask = ({ select, onSelect }: SelectTaskProps) => {
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const lists: string[] = ["This Week", "Last Week"];
+
+  const selectedList = (
+    <>
+      <p>{select ? select : "Select Week"}</p>
+      <img src={arrow} alt={select} />
+    </>
+  );
+
+  return (
+    <div className="flex items-center justify-between">
+      <p>Activity</p>
+      <div className="text-xs relative">
+        <div
+          onClick={() => setIsShow((prevShow: boolean) => !prevShow)}
+          className="flex items-center gap-2 cursor-pointer"
+        >
+          {selectedList}
+        </div>
+        {isShow && (
+          <ul className="z-10 mt-2 left-0 absolute bg-primary-100 w-max border border-secondary-100 rounded-xl flex flex-col overflow-hidden">
+            {lists.map((list: string) => (
+              <li
+                key={list}
+                className="cursor-pointer p-2.5 hover:bg-tertiary-100 hover:text-primary-100 transition-all duration-200"
+                onClick={() => {
+                  onSelect(list);
+                  setIsShow(false);
+                }}
+              >
+                {list}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
