2be61c03148ea827e806037869e1b81404597e14
pujiragil <ragilsg164@gmail.com>
2023-03-07 21:37:58 +0700

    added breakpoints & slidesPerView for different screen size

--- src/components/Slider/Mentor/index.tsx
+++ src/components/Slider/Mentor/index.tsx
@@ -8,7 +8,39 @@ import { Mentor } from "../../Card";
 
 const MentorSlider = () => {
   return (
-    <Swiper modules={[Navigation]} slidesPerView={1}>
+    <Swiper
+      modules={[Navigation]}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 32,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 32,
+        },
+      }}
+    >
+      <SwiperSlide>
+        <Mentor
+          avatar="/profile.png"
+          name="Jokowi banteng"
+          jobTitle="Presiden Wakanda"
+          tasks={40}
+          stars={4.0}
+          reviews={750}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Mentor
+          avatar="/profile.png"
+          name="Jokowi banteng"
+          jobTitle="Presiden Wakanda"
+          tasks={40}
+          stars={4.0}
+          reviews={750}
+        />
+      </SwiperSlide>
       <SwiperSlide>
         <Mentor
           avatar="/profile.png"
--- src/components/Slider/Task/index.tsx
+++ src/components/Slider/Task/index.tsx
@@ -1,14 +1,38 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 
-import "swiper/css"
-import "swiper/css/navigation"
+import "swiper/css";
+import "swiper/css/navigation";
 
 import { Task } from "../../Card";
 
 const TaskSlider = () => {
   return (
-    <Swiper modules={[Navigation]} slidesPerView={1}>
+    <Swiper
+      modules={[Navigation]}
+      breakpoints={{
+        320: { slidesPerView: 1, spaceBetween: 32 },
+        1024: { slidesPerView: 2, spaceBetween: 32 },
+      }}
+    >
+      <SwiperSlide>
+        <Task
+          thumbnail="/task.png"
+          title="Creating Awesome Mobile Apps"
+          category="UI UX Design"
+          progress={75}
+          deadline={3}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Task
+          thumbnail="/task.png"
+          title="Creating Perfect Website"
+          category="Web Developer"
+          progress={85}
+          deadline={4}
+        />
+      </SwiperSlide>
       <SwiperSlide>
         <Task
           thumbnail="/task.png"
