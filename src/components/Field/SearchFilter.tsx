81f31118fcb8c7d705a166690338b3fa1f1db382
pujiragil <ragilsg164@gmail.com>
2023-03-08 22:12:05 +0700

    add padding

--- src/components/Field/SearchFilter.tsx
+++ src/components/Field/SearchFilter.tsx
@@ -1,8 +1,8 @@
 const SearchFilter = () => {
   return (
-    <div className="flex items-center gap-6 px-6 pb-6">
+    <div className="flex items-center gap-6 px-6 pb-6 md:p-0">
       <input
-        className="outline-none py-3.5 px-7 rounded-xl border border-primary-300 w-full h-[52px] focus:ring-1 focus:ring-primary-300"
+        className="outline-none bg-transparent py-3.5 px-7 rounded-xl border border-primary-300 w-full h-[52px] focus:ring-1 focus:ring-primary-300"
         placeholder="Search Mentors"
       />
       <div className="min-w-[52px] h-[52px] w-[52px] rounded-xl border border-primary-300 flex items-center justify-center hover:bg-primary-300 cursor-pointer">
--- src/pages/mentors.tsx
+++ src/pages/mentors.tsx
@@ -6,6 +6,7 @@ const Mentors = () => {
     <>
       <ProfileLayout
         titleProfile="Explore Mentors"
+        background="primary"
         children={<SearchFilter />}
       />
     </>
