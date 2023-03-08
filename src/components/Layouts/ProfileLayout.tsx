82da6846dd1a29c8c167fe2cce5e4c483c2b5ad5
pujiragil <ragilsg164@gmail.com>
2023-03-08 22:10:58 +0700

    added new props

--- src/components/Layouts/ProfileLayout.tsx
+++ src/components/Layouts/ProfileLayout.tsx
@@ -5,17 +5,27 @@ interface ProfileLayoutProps {
   titleProfile: string;
   descProfile?: string;
   children?: ReactNode;
+  background?: "primary";
 }
 
 const ProfileLayout: FC<ProfileLayoutProps> = ({
   titleProfile,
   descProfile,
   children,
+  background,
 }): ReactElement => {
   return (
-    <div className="bg-primary-100 flex flex-col gap-8 border-b border-primary-300 md:bg-primary-200 md:p-8 md:gap-6 md:border-none">
+    <div
+      className={`${background === "primary"
+          ? "bg-primary-100"
+          : "bg-primary-100 md:bg-primary-200"
+        } flex flex-col gap-8 border-b border-primary-300 md:p-8 md:gap-6 md:border-none`}
+    >
       <div className="flex flex-col gap-8 md:gap-0 md:flex-row md:justify-between md:items-center">
-        <div className={`${children ? null : "pb-8"} px-6 space-y-2 order-2 md:order-1 md:p-0`}>
+        <div
+          className={`${children ? null : "pb-8"
+            } px-6 space-y-2 order-2 md:order-1 md:p-0`}
+        >
           <h1 className="text-secondary-100 font-semibold text-2xl">
             {titleProfile}
           </h1>
