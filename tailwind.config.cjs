afe4a163087dd1fb34c50a32b480016241090d4d
pujiragil <ragilsg164@gmail.com>
2023-03-08 22:00:34 +0700

    fix: extend colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,23 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: {
-        100: "#FFFFFF",
-        200: "#FAFAFA",
-        300: "#F5F5F7",
-      },
-      secondary: {
-        100: "#141522",
-        200: "#54577A",
-        300: "#8E92BC",
-      },
-      tertiary: {
-        100: "#546FFF",
-        200: "#BAC8FF",
-      },
-    },
     extend: {
+      colors: {
+        primary: {
+          100: "#FFFFFF",
+          200: "#FAFAFA",
+          300: "#F5F5F7",
+        },
+        secondary: {
+          100: "#141522",
+          200: "#54577A",
+          300: "#8E92BC",
+        },
+        tertiary: {
+          100: "#546FFF",
+          200: "#BAC8FF",
+        },
+      },
       maxWidth: {
         screen: {
           "2xl": "1440px",
