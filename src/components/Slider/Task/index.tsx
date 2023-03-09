8f01126f7cdd5e51890025ac92064ea356a07e39
pujiragil <ragilsg164@gmail.com>
2023-03-09 16:23:33 +0700

    add props for taskSlider component

--- src/components/Slider/Task/index.tsx
+++ src/components/Slider/Task/index.tsx
@@ -4,35 +4,31 @@ import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 
-import { Task } from "../../Card";
+import { FC, ReactElement } from "react";
 
-const TaskSlider = () => {
+interface TaskSliderProps {
+  smSlide: number;
+  lgSlide: number;
+  sliders: ReactElement[];
+}
+
+const TaskSlider: FC<TaskSliderProps> = ({
+  smSlide,
+  lgSlide,
+  sliders,
+}): ReactElement => {
   return (
     <Swiper
+      className="task-slider"
       modules={[Navigation]}
       breakpoints={{
-        320: { slidesPerView: 1, spaceBetween: 32 },
-        1024: { slidesPerView: 2, spaceBetween: 32 },
+        320: { slidesPerView: smSlide, spaceBetween: 32 },
+        1024: { slidesPerView: lgSlide, spaceBetween: 32 },
       }}
     >
-      <SwiperSlide>
-        <Task
-          thumbnail="/task.png"
-          title="Creating Awesome Mobile Apps"
-          category="UI UX Design"
-          progress={75}
-          deadline={3}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Task
-          thumbnail="/task.png"
-          title="Creating Perfect Website"
-          category="Web Developer"
-          progress={85}
-          deadline={4}
-        />
-      </SwiperSlide>
+      {sliders.map((slide, index) => (
+        <SwiperSlide key={index}>{slide}</SwiperSlide>
+      ))}
     </Swiper>
   );
 };
