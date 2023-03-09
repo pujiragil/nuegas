c9ddd61740e728ced9f2d9d94869c9501e315d92
pujiragil <ragilsg164@gmail.com>
2023-03-09 17:41:35 +0700

    created utils

---
+++ src/utils/mentor.tsx
@@ -0,0 +1,112 @@
+import { ReactElement } from "react";
+import { Mentor } from "../components/Card";
+
+const mentorSliders: ReactElement[] = [
+  <Mentor
+    avatar="/profile.png"
+    name="Jessica Jane"
+    jobTitle="Frontend Dev"
+    tasks={40}
+    stars={3.7}
+    reviews={230}
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Jess No Unlimited"
+    jobTitle="Frontend Dev"
+    tasks={40}
+    stars={3.7}
+    reviews={230}
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Puan Maharaja"
+    jobTitle="Frontend Dev"
+    tasks={40}
+    stars={3.7}
+    reviews={230}
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Joko Pudidi"
+    jobTitle="Frontend Dev"
+    tasks={40}
+    stars={3.7}
+    reviews={230}
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Young Lex"
+    jobTitle="Frontend Dev"
+    tasks={40}
+    stars={3.7}
+    reviews={230}
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Masbro"
+    jobTitle="Frontend Dev"
+    tasks={40}
+    stars={3.7}
+    reviews={230}
+  />,
+];
+
+const mentorLists: ReactElement[] = [
+  <Mentor
+    avatar="/profile.png"
+    name="Budi Suka Bapak"
+    jobTitle="Backend Dev"
+    tasks={32}
+    stars={1.5}
+    reviews={10}
+    description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Rudi Bengkel"
+    jobTitle="Frontend Dev"
+    tasks={22}
+    stars={4.5}
+    reviews={100}
+    description="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Siska Alkohl"
+    jobTitle="Fullstack Dev"
+    tasks={52}
+    stars={3.5}
+    reviews={120}
+    description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Andi Begal"
+    jobTitle="UI UX Designer"
+    tasks={2}
+    stars={1.7}
+    reviews={20}
+    description="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Santi Beras"
+    jobTitle="Cyber Security"
+    tasks={34}
+    stars={2.5}
+    reviews={110}
+    description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+  />,
+  <Mentor
+    avatar="/profile.png"
+    name="Budi Suka Bapak"
+    jobTitle="Backend Dev"
+    tasks={32}
+    stars={1.5}
+    reviews={19}
+    description="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
+  />,
+];
+
+export { mentorSliders, mentorLists };
