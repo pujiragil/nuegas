6a886d0dbfa4a626e346122c15f3cc1824eeac63
pujiragil <ragilsg164@gmail.com>
2023-03-10 09:02:28 +0700

    added taskSlider component

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -1,9 +1,30 @@
+import SearchFilter from "../components/Field/SearchFilter";
 import ProfileLayout from "../components/Layouts/ProfileLayout";
+import TaskSlider from "../components/Slider/Task";
+import { taskSliders } from "../utils/task";
 
 const Task = () => {
   return (
     <>
-      <ProfileLayout titleProfile="Explore Task"></ProfileLayout>
+      <ProfileLayout
+        titleProfile="Explore Task"
+        background="primary"
+        children={<SearchFilter placeholder="Search Task" />}
+      />
+      <div className="flex flex-col gap-8 px-6 py-8 md:p-8">
+        <TaskSlider
+          heading="Time Limit"
+          smSlide={1}
+          lgSlide={3}
+          sliders={taskSliders}
+        />
+        <TaskSlider
+          heading="New Task"
+          smSlide={1}
+          lgSlide={3}
+          sliders={taskSliders}
+        />
+      </div>
     </>
   );
 };
