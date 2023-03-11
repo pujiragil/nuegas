af05c18ba31a0a535699b59872b0aa1ebac5ce88
pujiragil <ragilsg164@gmail.com>
2023-03-11 13:34:06 +0700

    fix: template literal classname

--- src/components/Atoms/Head.tsx
+++ src/components/Atoms/Head.tsx
@@ -1,23 +1,25 @@
 import { createElement, FC, ReactElement } from "react";
 
 interface HeadProps {
-  type: "h1" | "h2" | "h3" | "h4",
-  fontSize: [string, string] | string,
-  fontWeight: "font-base" | "font-medium" | "font-semibold",
-  value: string
+  type: "h1" | "h2" | "h3" | "h4";
+  fontSize: [string, string] | string;
+  fontWeight: "font-base" | "font-medium" | "font-semibold";
+  value: string;
 }
 
-const Head: FC<HeadProps> = ({ type, fontSize, fontWeight, value }): ReactElement => {
-  let className: string;
-
-  if (fontSize.length && typeof fontSize === "object") {
-    const [smSize, mdSize]: string[] = fontSize;
-    className = `text-secondary-100 ${fontWeight} ${smSize} md:${mdSize}`;
-  } else {
-    className = `text-secondary-100 ${fontWeight} ${fontSize}`
-  }
+const Head: FC<HeadProps> = ({
+  type,
+  fontSize,
+  fontWeight,
+  value,
+}): ReactElement => {
+  let className: string = [
+    "text-secondary-100",
+    fontWeight,
+    typeof fontSize === "string" ? fontSize : fontSize.join(" "),
+  ].join(" ");
 
   return createElement(type, { className }, value);
-}
+};
 
 export default Head;
--- src/components/Atoms/index.tsx
+++ src/components/Atoms/index.tsx
@@ -3,3 +3,4 @@ export { default as Description } from "./Description";
 export { default as Thumbnail } from "./Thumbnail";
 export { default as Duration } from "./Duration";
 export { default as Student } from "./Student";
+export { default as Head } from "./Head";
