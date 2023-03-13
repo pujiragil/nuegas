b10485590f7117b51a40a633fe5fb04c330c4fe3
pujiragil <ragilsg164@gmail.com>
2023-03-13 22:39:32 +0700

    change ChatUserLink props

--- src/pages/Message/index.tsx
+++ src/pages/Message/index.tsx
@@ -1,4 +1,7 @@
 import ProfileLayout from "../../components/Layouts/ProfileLayout";
+import { NavLink, Outlet } from "react-router-dom";
+import { FC } from "react";
+import messages from "../../utils/message";
 
 const Message = () => {
   return (
@@ -33,77 +36,48 @@ const Message = () => {
           </div>
 
           <div className="flex flex-col divide-y divide-primary-300">
-            <ChatPerson isActive={true} />
-            <ChatPerson isActive={false} />
-            <ChatPerson isActive={false} />
-            <ChatPerson isActive={false} />
-            <ChatPerson isActive={false} />
-            <ChatPerson isActive={false} />
-            <ChatPerson isActive={false} />
-            <ChatPerson isActive={false} />
-          </div>
-        </div>
-
-        <div className="hidden md:block md:w-8/12">
-          <div className="bg-primary-100 py-6 px-12 flex items-center justify-between">
-            <div className="flex items-center gap-3">
-              <img
-                className="w-[52px] h-[52px] object-cover rounded-full"
-                src="/profile.png"
-                alt="profile"
+            {messages.map((message) => (
+              <ChatUserLink
+                key={message.id}
+                link={message.link}
+                user={message.user}
               />
-              <div className="space-y-2">
-                <p className="font-semibold text-sm text-secondary-100">
-                  Angel Saris
-                </p>
-                <div className="flex items-center gap-2">
-                  <span className="w-2 h-2 rounded-full bg-[#25C78B]"></span>
-                  <p className="font-semibold text-xs text-secondary-100">
-                    Online
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex items-center gap-6">
-              <div className="h-[52px] w-[52px] border border-primary-300 rounded-full flex items-center justify-center cursor-pointer hover:bg-primary-200 transition-all ease-in-out">
-                <img
-                  className="w-6 h-6 object-cover"
-                  src="/video-call.svg"
-                  alt="video-call"
-                />
-              </div>
-              <div className="h-[52px] w-[52px] border border-primary-300 rounded-full flex items-center justify-center cursor-pointer hover:bg-primary-200 transition-all ease-in-out">
-                <img
-                  className="w-6 h-6 object-cover"
-                  src="/voice-call.svg"
-                  alt="voice-call"
-                />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
+
+        <Outlet />
       </div>
     </>
   );
 };
 
-const ChatPerson = ({ isActive }: { isActive: boolean }) => {
+interface ChatUserLinkProps {
+  user: {
+    avatar: string;
+    name: string;
+    isOnline: boolean;
+  };
+  link: string;
+}
+
+const ChatUserLink: FC<ChatUserLinkProps> = ({ user, link }) => {
   return (
     <div className="py-4 first:pt-0 last:pb-0">
-      <div
-        className={`flex items-center gap-3 py-2.5 px-5 rounded-xl ${isActive ? "bg-primary-200" : null
+      <NavLink
+        to={link}
+        className={`flex items-center gap-3 py-2.5 px-5 rounded-xl ${user.isOnline ? "bg-primary-200" : null
           }`}
       >
         <img
           className="w-12 h-12 object-cover rounded-full"
-          src="/profile.png"
+          src={user.avatar}
           alt="profile"
         />
         <div className="space-y-2 w-full">
           <div className="flex items-center justify-between">
             <p className="font-semibold text-sm text-secondary-100">
-              Angel Saris
+              {user.name}
             </p>
             <p className="font-base text-xs text-secondary-300">1m Ago</p>
           </div>
@@ -111,10 +85,14 @@ const ChatPerson = ({ isActive }: { isActive: boolean }) => {
             <p className="font-base text-xs text-secondary-100">
               Thank you very much...
             </p>
-            <span className="w-2 h-2 rounded-full bg-[#DB5962]"></span>
+            {user.isOnline ? (
+              <span className="w-2 h-2 rounded-full bg-[#25C78B]"></span>
+            ) : (
+              <span className="w-2 h-2 rounded-full bg-[#DB5962]"></span>
+            )}
           </div>
         </div>
-      </div>
+      </NavLink>
     </div>
   );
 };
