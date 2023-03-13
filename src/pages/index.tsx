40ad96ccb1ea4cd46260c58d24ef3296a7e180d9
pujiragil <ragilsg164@gmail.com>
2023-03-13 22:39:56 +0700

    export message room page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,4 +4,5 @@ export { default as Task } from "./Task";
 export { default as TaskDetail } from "./Task/TaskDetail";
 export { default as Mentors } from "./Mentors";
 export { default as Message } from "./Message";
+export { default as MessageRoom } from "./Message/MessageRoom";
 export { default as Settings } from "./Settings";
