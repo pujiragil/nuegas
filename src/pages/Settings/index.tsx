fee10ae58b423a70e165f57e708a4a3e40d2a20e
pujiragil <ragilsg164@gmail.com>
2023-03-14 21:43:51 +0700

    added state for timezone

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,8 +1,14 @@
 import ProfileLayout from "../../components/Layouts/ProfileLayout";
 import SwitchTab from "../../components/Switch";
 import dropdown from "../../assets/arrow.svg";
+import { useState } from "react";
+
+const timezoneWrapperStyle: string =
+  "cursor-pointer py-4 px-5 rounded-xl border flex items-center gap-4";
 
 const Settings = () => {
+  const [timeZone, setTimeZone] = useState<"12 hour" | "24 hour">("24 hour");
+
   return (
     <>
       <ProfileLayout
@@ -41,13 +47,36 @@ const Settings = () => {
             <div className="flex flex-col gap-4">
               <p className="font-semibold text-sm">Timezone</p>
               <div className="flex items-center gap-4">
-                <div className="py-4 px-5 rounded-xl border border-tertiary-100 flex items-center gap-4">
+                <div
+                  onClick={() => setTimeZone("24 hour")}
+                  className={`${timeZone === "24 hour"
+                      ? "border-tertiary-100"
+                      : "border-primary-300"
+                    } ${timezoneWrapperStyle}`}
+                >
                   <p className="font-medium text-xs">24 Hours</p>
-                  <span className="w-5 h-5 rounded-full border-4 border-tertiary-100"></span>
+                  <span
+                    className={`${timeZone === "24 hour"
+                        ? "border-4 border-tertiary-100"
+                        : "border-2 border-primary-300"
+                      } w-5 h-5 rounded-full`}
+                  ></span>
                 </div>
-                <div className="py-4 px-5 rounded-xl border border-primary-300 flex items-center gap-4">
+
+                <div
+                  onClick={() => setTimeZone("12 hour")}
+                  className={`${timeZone === "12 hour"
+                      ? "border-tertiary-100"
+                      : "border-primary-300"
+                    } ${timezoneWrapperStyle}`}
+                >
                   <p className="font-medium text-xs">12 Hours</p>
-                  <span className="w-5 h-5 rounded-full border-2 border-primary-300"></span>
+                  <span
+                    className={`${timeZone === "12 hour"
+                        ? "border-4 border-tertiary-100"
+                        : "border-2 border-primary-300"
+                      } w-5 h-5 rounded-full`}
+                  ></span>
                 </div>
               </div>
             </div>
