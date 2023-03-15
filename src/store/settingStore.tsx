9244c58b7f0d16b10b5dde7da16183704a3d1136
pujiragil <ragilsg164@gmail.com>
2023-03-15 21:53:21 +0700

    store: change setting store with slice pattern

--- src/store/settingStore.tsx
+++ src/store/settingStore.tsx
@@ -1,33 +1,11 @@
-import { create } from "zustand";
+import { create, StateCreator } from "zustand";
 
-type State = {
-  languages: string[];
-  timezones: string[];
-  timezoneTypes: string[];
-  selectedLanguage: string;
-  selectedTimezone: string;
-  selectedTimezoneType: string;
-};
+interface TabSlice {
+  selectedTab: string;
+  setSelectedTab: (tab: string) => void;
+}
 
-type Action = {
-  updateSelectedLanguage: (language: State["selectedLanguage"]) => void;
-  updateSelectedTimezone: (timezone: State["selectedTimezone"]) => void;
-  updateSelectedTimezoneType: (
-    timezoneType: State["selectedTimezoneType"]
-  ) => void;
-};
-
-const useSetting = create<State & Action>((set, get) => ({
-  languages: ["English", "Indonesian", "Spain", "Java"],
-  timezones: ["English", "Indonesian", "Spain", "Java"],
-  timezoneTypes: ["24 hour", "12 hour"],
-  selectedLanguage: get().languages[0],
-  selectedTimezone: get().timezones[0],
-  selectedTimezoneType: get().timezoneTypes[0],
-  updateSelectedLanguage: (language) => set({ selectedLanguage: language }),
-  updateSelectedTimezone: (timezone) => set({ selectedTimezone: timezone }),
-  updateSelectedTimezoneType: (timezoneType) =>
-    set({ selectedTimezoneType: timezoneType }),
-}));
-
-export default useSetting;
+const createTabSlice: StateCreator<TabSlice, [], [], TabSlice> = (set) => ({
+  selectedTab: "general",
+  setSelectedTab: (tab) => set({ selectedTab: tab }),
+});
