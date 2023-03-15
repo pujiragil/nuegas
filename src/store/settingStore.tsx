f47ce4750664e18aa38ea87c08edc605716403c7
pujiragil <ragilsg164@gmail.com>
2023-03-15 21:58:22 +0700

    store: create the general slice

--- src/store/settingStore.tsx
+++ src/store/settingStore.tsx
@@ -9,3 +9,23 @@ const createTabSlice: StateCreator<TabSlice, [], [], TabSlice> = (set) => ({
   selectedTab: "general",
   setSelectedTab: (tab) => set({ selectedTab: tab }),
 });
+
+interface GeneralSlice {
+  language: string;
+  timezone: string;
+  timezoneType: number;
+  setLanguage: (language: string) => void;
+  setTimezone: (timezone: string) => void;
+  setTimezoneType: (timezoneType: number) => void;
+}
+
+const createGeneralSlice: StateCreator<GeneralSlice, [], [], GeneralSlice> = (
+  set
+) => ({
+  language: "English",
+  timezone: "English",
+  timezoneType: 24,
+  setLanguage: (language) => set({ language }),
+  setTimezone: (timezone) => set({ timezone }),
+  setTimezoneType: (timezoneType) => set({ timezoneType }),
+});
