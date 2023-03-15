0161cbcba1ae78ab2d3b9696f714666ca3047f57
pujiragil <ragilsg164@gmail.com>
2023-03-15 22:07:58 +0700

    store: create notification slice & useSettingStore

--- src/store/settingStore.tsx
+++ src/store/settingStore.tsx
@@ -29,3 +29,40 @@ const createGeneralSlice: StateCreator<GeneralSlice, [], [], GeneralSlice> = (
   setTimezone: (timezone) => set({ timezone }),
   setTimezoneType: (timezoneType) => set({ timezoneType }),
 });
+
+interface NotificationSlice {
+  message: boolean;
+  taskUpdate: boolean;
+  taskDeadline: boolean;
+  mentorHelp: boolean;
+  setMessage: (show: boolean) => void;
+  setTaskUpdate: (show: boolean) => void;
+  setTaskDeadline: (show: boolean) => void;
+  setMentorHelp: (show: boolean) => void;
+}
+
+const createNotificationSlice: StateCreator<
+  NotificationSlice,
+  [],
+  [],
+  NotificationSlice
+> = (set) => ({
+  message: true,
+  taskUpdate: false,
+  taskDeadline: true,
+  mentorHelp: false,
+  setMessage: (message) => set({ message }),
+  setTaskUpdate: (taskUpdate) => set({ taskUpdate }),
+  setTaskDeadline: (taskDeadline) => set({ taskDeadline }),
+  setMentorHelp: (mentorHelp) => set({ mentorHelp }),
+});
+
+const useSettingStore = create<TabSlice & GeneralSlice & NotificationSlice>()(
+  (...a) => ({
+    ...createTabSlice(...a),
+    ...createGeneralSlice(...a),
+    ...createNotificationSlice(...a),
+  })
+);
+
+export default useSettingStore;
