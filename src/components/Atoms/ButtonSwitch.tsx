b08e6ab068222be45f77e2e2c0d020248829773f
pujiragil <ragilsg164@gmail.com>
2023-03-16 21:21:00 +0700

    atoms: changes ButtonSwitch prop

--- src/components/Atoms/ButtonSwitch.tsx
+++ src/components/Atoms/ButtonSwitch.tsx
@@ -5,17 +5,14 @@ interface ButtonSwitchProps {
 }
 
 const ButtonSwitch: FC<ButtonSwitchProps> = ({ isActive }): ReactElement => {
-  const [active, setActive] = useState<boolean>(isActive);
-
   return (
     <div
-      onClick={() => setActive((prev) => !prev)}
       className="w-[55px] h-[30px] rounded-full border border-primary-300 p-[5px] cursor-pointer"
     >
       <span
-        className={`${active
-            ? "translate-x-full bg-tertiary-100"
-            : "translate-x-0 bg-primary-300"
+        className={`${isActive
+          ? "translate-x-full bg-tertiary-100"
+          : "translate-x-0 bg-primary-300"
           } w-5 h-5 block rounded-full duration-300 ease-in`}
       ></span>
     </div>
