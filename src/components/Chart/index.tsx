b5e89f5e3256257298362da4986c22a17d5f3782
pujiragil <ragilsg164@gmail.com>
2023-03-17 16:33:01 +0700

    utils: create dummy taskList

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,49 +1,11 @@
 import arrow from "../../assets/arrow.svg";
 import ActivityChart from "./AcitivityChart";
 import React, { useState } from "react";
+import { taskList } from "../../utils/task";
 
-type List = {
-  day: string;
-  task: number;
-};
-
-type TaskList = {
-  "this week": Array<List>;
-  "last week": Array<List>;
-  default: Array<List>;
-};
-
-const taskList: TaskList = {
-  "this week": [
-    { day: "S", task: 4 },
-    { day: "M", task: 12 },
-    { day: "T", task: 5 },
-    { day: "W", task: 7 },
-    { day: "TH", task: 3 },
-    { day: "F", task: 6 },
-    { day: "ST", task: 2 },
-  ],
-  "last week": [
-    { day: "S", task: 7 },
-    { day: "M", task: 2 },
-    { day: "T", task: 6 },
-    { day: "W", task: 4 },
-    { day: "TH", task: 12 },
-    { day: "F", task: 3 },
-    { day: "ST", task: 3 },
-  ],
-  default: [
-    { day: "S", task: 0 },
-    { day: "M", task: 0 },
-    { day: "T", task: 0 },
-    { day: "W", task: 0 },
-    { day: "TH", task: 0 },
-    { day: "F", task: 0 },
-    { day: "ST", task: 0 },
-  ],
-};
 const Chart = () => {
   const [selectList, setSelectedList] = useState<string>("");
+
   const datas = () => {
     if (!selectList) {
       return taskList["default"];
--- src/utils/task.tsx
+++ src/utils/task.tsx
@@ -32,4 +32,45 @@ const taskSliders: ReactElement[] = [
   />,
 ];
 
-export { taskSliders };
+type List = {
+  day: string;
+  task: number;
+};
+
+type TaskList = {
+  "this week": Array<List>;
+  "last week": Array<List>;
+  default: Array<List>;
+};
+
+const taskList: TaskList = {
+  "this week": [
+    { day: "S", task: 4 },
+    { day: "M", task: 12 },
+    { day: "T", task: 5 },
+    { day: "W", task: 7 },
+    { day: "TH", task: 3 },
+    { day: "F", task: 6 },
+    { day: "ST", task: 2 },
+  ],
+  "last week": [
+    { day: "S", task: 7 },
+    { day: "M", task: 2 },
+    { day: "T", task: 6 },
+    { day: "W", task: 4 },
+    { day: "TH", task: 12 },
+    { day: "F", task: 3 },
+    { day: "ST", task: 3 },
+  ],
+  default: [
+    { day: "S", task: 0 },
+    { day: "M", task: 0 },
+    { day: "T", task: 0 },
+    { day: "W", task: 0 },
+    { day: "TH", task: 0 },
+    { day: "F", task: 0 },
+    { day: "ST", task: 0 },
+  ],
+};
+
+export { taskSliders, taskList };
