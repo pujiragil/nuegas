d6c458e6c27ec6b631d7328b41464ec26d57d7ca
pujiragil <ragilsg164@gmail.com>
2023-03-18 17:47:16 +0700

    added new props for select event

--- src/components/Field/SelectDropdown.tsx
+++ src/components/Field/SelectDropdown.tsx
@@ -1,12 +1,21 @@
+import { FC } from "react";
+
 const listStyle: string =
   "px-5 py-4 cursor-pointer hover:bg-primary-300 transition-all duration-300 ease-in-out";
 
-const SelectDropdown = () => {
+interface SelectDropdownProps {
+  lists: string[];
+  onSelect: (language: string) => void;
+}
+
+const SelectDropdown: FC<SelectDropdownProps> = ({ lists, onSelect }) => {
   return (
     <div className="absolute top-full left-0 w-full mt-4 bg-primary-100 z-10 border border-primary-300 grid grid-cols-1 divide-y divide-primary-300 rounded-xl overflow-hidden font-medium text-xs text-secondary-100">
-      <p className={listStyle}>English (Default)</p>
-      <p className={listStyle}>Indonesia</p>
-      <p className={listStyle}>Javanese</p>
+      {lists.map((list) => (
+        <p key={list} onClick={() => onSelect(list)} className={listStyle}>
+          {list}
+        </p>
+      ))}
     </div>
   );
 };
